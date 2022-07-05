46daf4ecbee81534491f477642e774bbf44c172a
4nds <andrej.slapnicar@gmail.com>
2022-07-05 03:07:35 +0200

    dodan matrica za evaluaciju greske

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,4 +1,10 @@
+import warnings
 import numpy as np              # 1.22.0
+import pandas as pd             # 1.3.4
+#from pandas.core.common import SettingWithCopyWarning
+
+warnings.simplefilter(action="ignore", \
+    category=pd.core.common.SettingWithCopyWarning)
 
 class Evaluator:
 
@@ -42,3 +48,35 @@ class Evaluator:
         maximal_score = (0.25*np.sum(actual_stances == 'unrelated')
             + np.sum(actual_stances != 'unrelated'))
         return score / maximal_score
+
+    def getScoreMatrix(self, actual_stances, predicted_stance):
+        stances = pd.DataFrame(data=dict(
+            ActualStances=actual_stances,
+            PredictedStance=predicted_stance,
+        ))
+        score_matrix = pd.DataFrame(dict({
+            '': ['Agree', 'Disagree', 'Discuss', 'Unrelated', 'Overall'],
+            'Agree': np.zeros(5, dtype=int),
+            'Disagree': np.zeros(5, dtype=int),
+            'Discuss': np.zeros(5, dtype=int),
+            'Unrelated': np.zeros(5, dtype=int),
+            'Accuracy (%)': np.zeros(5),
+        }))
+        stances_types = ['Agree', 'Disagree', 'Discuss', 'Unrelated']
+        for predicted_stance in stances_types:
+            for row in range(4):
+                actual_stance = stances_types[row]
+                #score_matrix.loc[:row, actual_stance] = \
+                #score_matrix.at[predicted_stance, row] = \
+                score_matrix[predicted_stance].iloc[row] = \
+                    np.sum((stances['ActualStances'] == actual_stance.lower()) & \
+                        (stances['PredictedStance'] == predicted_stance.lower()))
+        for row in range(4):
+            actual_stance = stances_types[row]
+            score_matrix['Accuracy (%)'].iloc[row] = \
+                round(100 * (score_matrix[actual_stance].iloc[row]
+                    / np.sum(score_matrix.iloc[row][stances_types])), 2)
+        score_matrix['Accuracy (%)'].iloc[4] = \
+            round(100 * (sum(score_matrix[stance].iloc[row] for row, stance \
+                in enumerate(stances_types)) / stances.shape[0]), 2)
+        return score_matrix
